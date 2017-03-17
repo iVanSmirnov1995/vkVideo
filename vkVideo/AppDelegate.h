fb0e9219d5f6e9505cd27ed8397bea151c384493
vaniok123 <ivan.smirnov.95@mail.ru>
2017-03-17 11:42:47 +0300

    Initial Commit

---
+++ vkVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vkVideo
+//
+//  Created by Smirnov Ivan on 17.03.17.
+//  Copyright © 2017 Smirnov Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
